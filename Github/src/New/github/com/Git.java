49349c8a407f405dcf8a4efeab72d4dfa77dbab3
RAVIKUMARBIDNOOR <rkbidnoor@gmail.com>
2017-03-22 18:48:16 +0530

    New comment

---
+++ Github/src/New/github/com/Git.java
@@ -0,0 +1,11 @@
+package New.github.com;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1st git hub ");
+
+	}
+
+}
---
+++ Selenium2/New1/Selenium_Execution/src/lib/com/Shorcuts.java
@@ -0,0 +1,29 @@
+package lib.com;
+
+import java.util.Iterator;
+
+import net.sourceforge.htmlunit.corejs.javascript.ast.ForInLoop;
+import net.sourceforge.htmlunit.corejs.javascript.ast.ForLoop;
+
+import org.apache.jasper.tagplugins.jstl.core.ForEach;
+
+public class Shorcuts {
+	private String name;
+	private String addess;
+	
+
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+	System.out.println("Hi");
+	System.out.println("Hello");
+	System.out.println("Ravi");
+			
+	}
+
+
+			
+		}
+
+
